93670db0c0f28e9f052f2a9b5467e79e88e75efd
CsabaVK <56678429+CsabaVK@users.noreply.github.com>
2019-11-12 15:02:11 +0100

    Add files via upload

---
+++ 11_12_GC/11_12_GC/11_12_GC/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//
+using System.Data.SqlClient;
+
+namespace _11_12_GC
+{
+    class Program
+    {
+        //SQL ADATBÁZIS
+        //https://www.connectionstrings.com/
+        //SQL Server
+        static void Main(string[] args)
+        {
+            //@ = Összes utasítás ignorálása
+            string connectionsString = @"Server = (localdb)\MSSQLLocalDB; Database=teszt";
+            //SQL Server Object Explorer --> Adatbázis, lenyit --> Properties (másik ablak) - Connections String
+
+            var connection = new SqlConnection(connectionsString);
+
+            connection.Open();
+
+            //SQL LEKÉRDEZÉS
+            var sql = new SqlCommand("select * from nevek;", connection);
+            //OLVASÓ
+            SqlDataReader reader = sql.ExecuteReader();
+
+            //PL: id, nev, ev
+            //Ha az olvasó "Read" visszatér igazzal akkor tudott olvasni
+            while (reader.Read())
+            {
+                //Console.WriteLine($"{reader[0]} {reader[1]} {reader[2]}");
+                //Console.WriteLine($"{reader[id]} {reader[nev]} {reader[ev]}");
+                Console.WriteLine($"{reader[0]} {reader["nev"]} {2019 - reader.GetInt32(2)}");
+            }
+            connection.Close();
+            Console.ReadKey();
+        }
+    }
+}
